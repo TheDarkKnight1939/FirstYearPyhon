9c55f38f41883fd584dec22afd15c1eeca036722
TheDarkKnight1939 <31588918+TheDarkKnight1939@users.noreply.github.com>
2017-09-14 15:16:23 +0530

    Update NumberType.py

--- NumberType.py
+++ NumberType.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-g=(int(input("Enter a number")))
-if (g>0):
+g=(int(input("Enter a number"))) #Asks the user to enter a number.
+if (g>0):                                 #using if else-if statements , checks if the number is positive , negaqtive or zero.
 	print(g, "Is a positive number")
 elif(g==0):
 	print("The number is zero")
