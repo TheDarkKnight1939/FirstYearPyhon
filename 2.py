df1edcd667dba615089197386384a4eed713526a
TheDarkKnight1939 <31588918+TheDarkKnight1939@users.noreply.github.com>
2017-09-28 14:45:00 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
-name="Real"
-name1="Madrid "
-print(name)
-print(name1)
-print(name*3)
-print(name1+" is the biggest team")
-print(name[0])
-print(name[1])
-print(name[2])
-print(name[3])
-print(name1[0])
-print(name1[1])
-print(name1[2])
-print(name1[3])
-print(name1[4])
-print(name1[5])
-print(name1[6])
+name="Real" #Stores the word Real in the variable name
+name1="Madrid " #Stores the word Madrid in the variable name1
+print(name) #Prints the String in the variable name
+print(name1) #Prints the string in the variable name1
+print(name*3) #Prints the string in variable name thrice
+print(name1+" is the biggest team") 
+print(name[0]) #Prints the 1st letter from the variable name
+print(name[1]) #Prints the 2nd letter from the variable name
+print(name[2]) #Prints the 3rd letter from the variable name
+print(name[3]) #Prints the 4th letter from the variable name
+print(name1[0]) #Prints the 5th letter from the variable name
+print(name1[1]) #Prints the 6th letter from the variable name
+print(name1[2]) #Prints the 7th letter from the variable name
+print(name1[3]) #Prints the 8th letter from the variable name
+print(name1[4]) #Prints the 9th letter from the variable name
+print(name1[5]) #Prints the 10th letter from the variable name
+print(name1[6]) #Prints the 11th letter from the variable name
