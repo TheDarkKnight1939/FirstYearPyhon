084d5db0b6870e60e8a4418c4dcbc95a816f3e1c
TheDarkKnight1939 <31588918+TheDarkKnight1939@users.noreply.github.com>
2017-09-28 14:48:41 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
-import random
-r=random.randint(10,66)
-print(r)
-if r<35:
+import random #Imports the class random
+r=random.randint(10,66) #Uses the randint function to choose a number from 10 to 66 
+print(r) #Prints the random number choosen
+if r<35: #If loop which checks if the number is lesser than 35 
 	print(r)
 	print(": is less than 35 \n")
 	exit
-elif r==30:
+elif r==30:#If loop which checks if the number is equal to 30
 	print(" 30 is multiple of 10 and 3, both ")
-elif r>=35:
+elif r>=35:#If loop which checks if the number is lesser than 35
 	print(r, " is greater than 35")
 else:
 	print("your number is : ", r)
