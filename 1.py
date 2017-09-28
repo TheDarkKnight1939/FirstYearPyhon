853abf6cb80512601e3e3e76f33694395acc77a8
TheDarkKnight1939 <31588918+TheDarkKnight1939@users.noreply.github.com>
2017-09-28 14:40:11 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3 
-a=45
-print(a)
-print(a+3)
-print(a-23)
-print(a/12)
-print(a%3) 
+a=45 #Initialises variable a to 45
+print(a) #prints the number in variable a
+print(a+3)  #adds 3 to variable a and prints it
+print(a-23) #Minuses 23 from variable a
+print(a/12) #Divides variable a by 12
+print(a%3)  #Finds the reminder of the number when it is divided by 3
+
 
 
